f316ca43f192494adb2c06601178e1e5fdaa0035
LiMIingzhe <limingzhe1110@gmail.com>
2024-09-22 23:23:00 +0800

    23:22

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 class number {
 public:
-    int up;
-    int down;
+    int up=0;
+    int down=1;
 
     int gcd(int a,int b) {
-        if(a%b==0)return b;
-        return gcd(a/b,a%b);
+        if(b==0)return a;
+        return gcd(b,a%b);
     }
     void cancelliing() {
         int thegcd=gcd(up,down);
@@ -50,7 +50,6 @@ number mulNumber(number a,number b) {
     c.cancelliing();
     return c;
 }
-
 class item :number{
 public:
     number coefficient;
@@ -143,13 +142,20 @@ public:
         highest_degree=max_degree;
     }
 };
+class divideResult {
+public:
+    polynomial q;
+    polynomial r;
+};
 polynomial AddPolynomial(polynomial a,polynomial b) {
     polynomial c;
     c.ini();
     for(int i=0;i<MAX_ITEM;i++) {
         c.theItem[i].coefficient=addNumber(a.theItem[i].coefficient,b.theItem[i].coefficient);
+        if(c.theItem[i].coefficient.up!=0)c.highest_degree=i;
     }
-    c.highest_degree=a.highest_degree>=b.highest_degree?a.highest_degree:b.highest_degree;
+    //c.highest_degree=a.highest_degree>=b.highest_degree?a.highest_degree:b.highest_degree;
+
     return c;
 }
 polynomial MultiplyPolynomial(polynomial a,polynomial b) {
@@ -172,6 +178,38 @@ polynomial SubtractPolynomial(polynomial a,polynomial b) {
     d.highest_degree=0;
     return AddPolynomial(a,MultiplyPolynomial(b,d));
 }
+divideResult DividePolynomial(polynomial a,polynomial b) {
+    polynomial result_q;
+    polynomial result_r;
+    result_q.ini();
+    result_r.ini();
+    result_r=a;
+    while(true) {
+        //result_q.PrintPolynomial(true);
+        //result_r.PrintPolynomial(true);
+        polynomial s;
+        s.highest_degree=result_r.highest_degree-b.highest_degree;
+        s.theItem[s.highest_degree].coefficient.up=b.theItem[b.highest_degree].coefficient.down*result_r.theItem[result_r.highest_degree].coefficient.up;
+        s.theItem[s.highest_degree].coefficient.down=b.theItem[b.highest_degree].coefficient.up*result_r.theItem[result_r.highest_degree].coefficient.down;
+        s.theItem[s.highest_degree].coefficient.cancelliing();
+        result_q=AddPolynomial(result_q,s);
+        result_r=SubtractPolynomial(result_r,MultiplyPolynomial(s,b));
+        if(result_r.highest_degree<b.highest_degree or result_r.highest_degree==0)break;
+    }
+    divideResult re;
+    re.q=result_q;
+    re.r=result_r;
+    return re;
+}
+polynomial GCD(polynomial a,polynomial b) {
+    divideResult re=DividePolynomial(a,b);
+    if(b.highest_degree==0) {
+        return a;
+    }
+    cout<<"(";a.PrintPolynomial(false);cout<<") = (";b.PrintPolynomial(false);cout<<") * (";re.q.PrintPolynomial(false);cout<<") + (";re.r.PrintPolynomial(false);cout<<")"<<endl;
+    return GCD(b,re.r);
+}
+
 int main() {
     polynomial a,b;
     a.ini();
@@ -182,11 +220,9 @@ int main() {
     cin>>sb;
     b.getPolyNomial(sb);
 
-    a.PrintPolynomial(true);
-    b.PrintPolynomial(true);
-
-    polynomial c=SubtractPolynomial(a,b);
-    c.PrintPolynomial(true);
-
+    //divideResult c=DividePolynomial(a,b);
+    //c.q.PrintPolynomial(true);
+    //c.r.PrintPolynomial(true);
+    GCD(a,b);
     return 0;
 }
